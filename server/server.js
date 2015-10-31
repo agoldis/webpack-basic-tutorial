fc1829bddba7a8f92c2c07b8374473e0dbc15083
Andrew Goldis <agoldis@gmail.com>
2015-11-01 05:06:12 +1100

    Static server is ready

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var port = 3000;
+var app = express();
+app.use(express.static(__dirname + '/../app')); // true static content
+app.listen(port, 'localhost', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+});
